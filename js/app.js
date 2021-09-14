e8fd1d10e2d4a8ff876fc7c61bcf51e86f52b8ff
AmyXandra <amyabafor013@gmail.com>
2021-09-14 15:20:00 +0100

    changes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
-/* Template Name: Invoza - Bootstrap 4 Landing Page Tamplate
-   Author: Themesdesign
+/* 
    File Description: Main JS file of the template
 */
 
 
-! function($) {
+! function ($) {
     "use strict";
 
-    var Invoza = function() {};
+    var Invoza = function () { };
 
-    Invoza.prototype.initStickyMenu = function() {
-        $(window).scroll(function() {
+    window.onscroll = function () { myFunction() };
+    var navbar = document.getElementById("navbar");
+
+    function myFunction() {
+        if (window.pageYOffset >= 50) {
+            $(".sticky").addClass("nav-sticky");
+        } else {
+            $(".sticky").removeClass("nav-sticky");
+        }
+    }
+
+
+    Invoza.prototype.initStickyMenu = function () {
+        $(window).onscroll(function () {
             var scroll = $(window).scrollTop();
-        
+
             if (scroll >= 50) {
                 $(".sticky").addClass("nav-sticky");
             } else {
@@ -21,92 +32,92 @@
         });
     },
 
-    Invoza.prototype.initSmoothLink = function() {
-        $('.navbar-nav a').on('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 0
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    },
+        Invoza.prototype.initSmoothLink = function () {
+            $('.navbar-nav a').on('click', function (event) {
+                var $anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $($anchor.attr('href')).offset().top - 0
+                }, 1500, 'easeInOutExpo');
+                event.preventDefault();
+            });
+        },
 
-    Invoza.prototype.initScrollspy = function() {
-        $("#navbarCollapse").scrollspy({
-            offset:20
-        });
-    },
+        Invoza.prototype.initScrollspy = function () {
+            $("#navbarCollapse").scrollspy({
+                offset: 20
+            });
+        },
 
-    Invoza.prototype.initTesticarousel = function() {
-        $('#testi-carousel').owlCarousel({
-            items: 1,
-            loop:true,
-            margin:10,
-            nav:true,
-            responsive:{
-                576:{
-                    items:2
-                },
-     
-            }
-        });
-    },
+        Invoza.prototype.initTesticarousel = function () {
+            $('#testi-carousel').owlCarousel({
+                items: 1,
+                loop: true,
+                margin: 10,
+                nav: true,
+                responsive: {
+                    576: {
+                        items: 2
+                    },
 
-    Invoza.prototype.initCounter = function() {
-        // Counter Number
-        var a = 0;
-        $(window).scroll(function() {
-            var oTop = $('#counter').offset().top - window.innerHeight;
-            if (a == 0 && $(window).scrollTop() > oTop) {
-                $('.counter-value').each(function() {
-                    var $this = $(this),
-                        countTo = $this.attr('data-count');
-                    $({
-                        countNum: $this.text()
-                    }).animate({
+                }
+            });
+        },
+
+        Invoza.prototype.initCounter = function () {
+            // Counter Number
+            var a = 0;
+            $(window).scroll(function () {
+                var oTop = $('#counter').offset().top - window.innerHeight;
+                if (a == 0 && $(window).scrollTop() > oTop) {
+                    $('.counter-value').each(function () {
+                        var $this = $(this),
+                            countTo = $this.attr('data-count');
+                        $({
+                            countNum: $this.text()
+                        }).animate({
                             countNum: countTo
                         },
 
-                        {
+                            {
 
-                            duration: 2000,
-                            easing: 'swing',
-                            step: function() {
-                                $this.text(Math.floor(this.countNum));
-                            },
-                            complete: function() {
-                                $this.text(this.countNum);
-                                //alert('finished');
-                            }
+                                duration: 2000,
+                                easing: 'swing',
+                                step: function () {
+                                    $this.text(Math.floor(this.countNum));
+                                },
+                                complete: function () {
+                                    $this.text(this.countNum);
+                                    //alert('finished');
+                                }
 
-                        });
-                });
-                a = 1;
-            }
-        });
+                            });
+                    });
+                    a = 1;
+                }
+            });
 
-    },
+        },
 
 
-    feather.replace()
+        feather.replace()
 
-    Invoza.prototype.init = function() {
+    Invoza.prototype.init = function () {
         this.initStickyMenu();
         this.initSmoothLink();
         this.initScrollspy();
         this.initTesticarousel();
         this.initCounter();
     },
-    //init
-    $.Invoza = new Invoza, $.Invoza.Constructor = Invoza
+        //init
+        $.Invoza = new Invoza, $.Invoza.Constructor = Invoza
 }(window.jQuery),
 
 
-//initializing
-function($) {
-    "use strict";
-    $.Invoza.init();
-}(window.jQuery);
+    //initializing
+    function ($) {
+        "use strict";
+        $.Invoza.init();
+    }(window.jQuery);
 
 
 
